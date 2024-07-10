951846407a8a271c0c79c403c39247def11dbc9a
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2024-07-10 13:33:50 +0200

    change portfolio item to be whole clickable

--- components/PortfolioItem/PortfolioItem.jsx
+++ components/PortfolioItem/PortfolioItem.jsx
@@ -3,21 +3,21 @@ import Image from "next/image";
 
 const PortfolioItem = ({ children, description, imgSrc, repositoryUrl }) => {
   return (
-    <div className="max-w-lg p-4 hover:bg-neutral-100/20 hover:scale-105 duration-500 rounded-xl flex flex-col items-center">
-      <h2 className="self-start">{children}</h2>
-      <p>{description}</p>
-      <Link href={repositoryUrl} target="_blank">
-        <div className="relative w-96 lg:w-[440px] aspect-video">
+    <Link href={repositoryUrl} target="_blank">
+      <div className="max-w-lg p-4 hover:bg-neutral-100/20 hover:scale-105 duration-500 rounded-xl flex flex-col items-center">
+        <h2 className="self-start">{children}</h2>
+        <p>{description}</p>
+
+        <div className="relative w-96 lg:w-[440px] aspect-video my-4">
           <Image
             layout="fill"
             objectFit="cover"
             src={imgSrc}
             alt="Project image"
-            className="mt-4"
           />
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
 
